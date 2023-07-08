f6dc2ce3427ac8499edf78914a7073f781a9cf3d
JulAleks <jalekseev@myseneca.ca>
2023-07-07 22:41:54 -0400

    updated h and c for input

--- SourceCode/project-starter/input.c
+++ SourceCode/project-starter/input.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include "input.h"
 
+
+
 //print out header onto console
 void header() {
 	printf("=================\n");
@@ -13,37 +15,60 @@ void header() {
 
 //get user input to initialize shipment weight
 void input() {
-	printf("Enter shipment weight, box size and destination(0 0 x to stop) :\n");
+	struct PackageInf pkg;
 	double weight;
 	double size;
 	char destination[4];
 	int valid = 0;
+
+	printf("Enter shipment weight, box size, and destination (0 0 x to stop):\n");
+	scanf("%lf, %lf, %3s", &weight, &size, destination);
+
+	
 	while (!valid) {
-		scanf("%d, %lf, %c", &weight, &size, destination);
+		scanf("%lf %lf %3s", &weight, &size, destination);
+		if (!validatePackageWeight(weight)) {
+			printf("Invalid weight (must be 1-1000 Kg.)\n");
+		}
+		else if (!validatePackageBox(size)) {
+			printf("Invalid size\n");
+		}
+		else if (!validateDestination(destination)) {
+			printf("Invalid destination\n");
+		}
+		else {
+			valid = 1;
+			pkg.m_boxSize = size; 
+			pkg.m_weight = weight;
+			strcpy(pkg.m_destination, destination);
+		}
 	}
+}
+
+//returns 1 weight if package within limits, else 0
+int validatePackageWeight(double num) {
+	return (num >= 1 && num <= 10) ? num : 0;
+}
+
+//returns 1 size if package within limits, else 0
+int validatePackageBox(double num) {
 	
-	//code to be continued in MS1
-	
+	return (num == SZMIN || num == SZMMID || num == SZMAX) ? num : 0;
 }
 
-//returns 1 if package within limits, else 0
-int validatePackageWeight() {
-	return 0;
-}
-
-//returns 1 if package within limits, else 0
-int validatePackageBox() {
-	return 0;
-}
-
-//returns 1 if destination  is valid, ('X' = building)
-int validateDestination() {
-	//array of valid building address destr, 3 for total entries, char inputs
-	//char *arr[] = {"7A", "8A", "12A", "13A", "14A", "15A", "16A", "2B", "3B", "7B", "8B"
-	//"22B", "23B", "24B", };
-
-
-	return 0;
+//returns the validated destination, ('X' = building)
+int validateDestination(char* dest) {
+	// Array of valid building addresses
+	char* arr[] = { "7A", "8A", "12A", "13A", "14A", "15A", "16A", "2B", "3B", "7B", "8B", "22B", "23B", "24B" };
+	int desNum = 14; // Number of destinations
+	int val = 0;
+	for (int i = 0; i < desNum; i++) {
+		if (strcmp(dest, arr[i]) == 0) {
+			val = 1;
+			i = desNum; //break out of loop
+		}
+	}
+	return val;
 }
 
 
--- SourceCode/project-starter/input.h
+++ SourceCode/project-starter/input.h
@@ -3,12 +3,12 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#define lowestWeight = 1 //kg
-#define highestWeight = 1000 //kg
+#define LOWWEIGHT 1      // kg
+#define HIGHWEIGHT 1000  // kg
 
-#define szMin = 0.25 //cubic meter
-#define szMid = 0.5	//cubic meter
-#define szMax = 1 //cubic meter
+#define SZMIN 0.25      // cubic meter
+#define SZMMID 0.5      // cubic meter
+#define SZMAX 1         // cubic meter
 
 struct PackageInf{
 	double m_weight;	//valid 1-1000 Kg.
@@ -16,20 +16,21 @@ struct PackageInf{
 	char m_destination[4]; //row num[1-25] and column letter [A-Y] Within the map
 };
 
+
 //print out header onto console
 void header();
 
 //get user input to enter package data related to weight, boxsize and destination, "0 0 x" to stop
 void input();
 
-//returns 1 if package within limits, else 0
-int validatePackageWeight();
+//returns 1 weight if package within limits, else 0
+int validatePackageWeight(double);
 
 //returns 1 if package within cubic meter limits, else 0
-int validatePackageBox();
+int validatePackageBox(double);
 
 //returns 1 if destination  is valid, ('X' = building re populateMap() for location of buildings from 'struct Map' )
-int validateDestination();
+int validateDestination(char* dest);
 
 
 #endif
