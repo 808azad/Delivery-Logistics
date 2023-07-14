fb1ad94d4e4a99a759133f7bd5c85a50c0a7bf62
JulAleks <jalekseev@myseneca.ca>
2023-07-14 17:44:41 -0400

    updated

--- SourceCode/project-starter/input.h
+++ SourceCode/project-starter/input.h
@@ -1,4 +1,15 @@
-//GROUP 5 SFT 
+/***********************************************************************
+//SFT GROUP 5 PROJECT
+
+Group members: 
+152953220 - TAE YONG EOM - tyeom@myseneca.ca
+051292134 - Julia Alekseev  - jalekseev@myseneca.ca
+123231227 - Chan Ka Ying - kchan151@myseneca.ca
+1122117229 - Azad Zeynalov - azeynalov1@myseneca.ca
+019153147 - Audrey Duzon - amduzon@myseneca.ca
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+
 
 #ifndef INPUT_H
 #define INPUT_H
@@ -17,22 +28,22 @@ struct PackageInf{
 };
 
 
-//print out header onto console
+//print out header onto console "Seneca Deliveries"
 void header();
 
-//print out footer onto consol
-void footer(); //NOT IMPLEMENTED HI TAEYON
+//print out footer onto consol, "Thank you messegae"
+void footer(); 
 
 //get user input to enter package data related to weight, boxsize and destination, "0 0 x" to stop
 void input(struct PackageInf *);
 
-//returns 1 weight if package within limits, else 0
+//returns 1 weight if package within limits (1-1000), else 0
 int validatePackageWeight(double);
 
-//returns 1 if package within cubic meter limits, else 0
+//returns 1 if package within cubic meter limits (0.25/0.5/1), else 0
 int validatePackageBox(double);
 
-//returns 1 if destination  is valid, ('X' = building re populateMap() for location of buildings from 'struct Map' )
+//returns 1 if destination  is valid(if the destination correspnds to the allowed destinations in the array, ('X' = building re populateMap() for location of buildings from 'struct Map' ), else return 0
 int validateDestination(char* dest); 
 
 
