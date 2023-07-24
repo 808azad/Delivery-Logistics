8cca253e819c86130c7dac26c489665281909be4
JulAleks <jalekseev@myseneca.ca>
2023-07-24 14:15:58 -0400

    UPDATED INPUT

--- SourceCode/project-starter/input.c
+++ SourceCode/project-starter/input.c
@@ -12,6 +12,8 @@ Group members:
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include "input.h"
@@ -38,91 +40,114 @@ int validatePackageBox(double num) {
 	return (num == SZMIN || num == SZMMID || num == SZMAX) ? 1 : 0;
 }
 
-//returns the validated destination, ('X' = building)
-int validateDestination(char* dest) {
-	// Array of second digit of valid building addresses
-	char arr[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', '\0' };
-	int desNum = 23; // Number of destinations (changed from 25 to 23 for the range [1, 23])
-	int val = 0;
+int isDestinationValid(const struct Map* routeMap, int destRow, int destCol) {
+    size_t maxRow = routeMap->numRows;
+    size_t maxCol = routeMap->numCols;
 
-	//if the length of the dest is 2
-	if (strlen(dest) == 2) {
-		// Check if the destination is valid
-		// Destination must be [NUM][ALPHABET] and NUM must be between 1 and 23 (inclusive), and ALPHABET must be between A and Y
-		if (isdigit(dest[0])) { // Check if the first character is digit
-			int num = dest[0] - '0'; // based on ASCII
+    if (destRow < 1 || destRow > maxRow || destCol < 1 || destCol > maxCol) {
+        return 0; // false, invalid row or column
+    }
 
-			if (num >= 1 && num <= desNum) { // Check if the integer is in the valid range [1, 23]
-				for (int i = 0; i < strlen(arr); i++) {
-					if (dest[1] == arr[i]) { // Check if the second character is a valid alphabet character
-						val = 1;
-						i = desNum; //break from loop 
-					}
-				}
-			}
-		}
-	}
-	//else if the length of the dest is 3
-	else if (strlen(dest) == 3) {
-		// Check if the destination is valid
-		// Destination must be [NUM][ALPHABET] and NUM must be between 1 and 23 (inclusive), and ALPHABET must be between A and Y
-		if (isdigit(dest[0]) && isdigit(dest[1])) { // Check if the first two characters are digits
-			int num1 = dest[0] - '0'; // based on ASCII
-			int num2 = dest[1] - '0'; // based on ASCII
-			int num = num1 * 10 + num2; // Convert the two digits to a two-digit integer
-
-			if (num >= 1 && num <= desNum) { // Check if the integer is in the valid range [1, 23]
-				int size = strlen(arr);
-				for (int i = 0; i < size; i++) {
-					if (dest[2] == arr[i]) { // Check if the third character is a valid alphabet character
-						val = 1;
-						i = desNum; //break from loop 
-					}
-				}
-			}
-		}
-	}
-	return val;
+    char destinationSymbol = routeMap->squares[destRow - 1][destCol - 1];
+    return (destinationSymbol != '#'); // true if not an obstacle (assuming '#' represents an obstacle)
 }
 
 
-//get user input to initialize shipment weight
-void input(struct PackageInf * pkg) {
-	double weight=0;
-	double size=0;
-	char destination[4]={ 0 };
-	int check = 0;
-	while (!check) {
-		printf("Enter shipment weight, box size, and destination (0 0 x to stop): ");
-		int check_input = scanf("%lf %lf %3s", &weight, &size, destination);
-		while (getchar() != '\n'); // Clear the input buffer
 
-		if (check_input != 3) {  //checks that the input receives 3 values
-			printf("Input failed\n");
-		}
-		else if(weight == 0 && size == 0 && destination[0] == 'x') {
-			check = 1;
-			pkg->m_boxSize = size; 
-			pkg->m_weight = weight;
-			strcpy(pkg->m_destination, destination);
-		}
-		else if (!validatePackageWeight(weight)) {
-			printf("Invalid weight (must be 1-1000 Kg.)\n");
-		}
-		else if (!validatePackageBox(size)) {
-			printf("Invalid size\n");
-		}
-		else if (!validateDestination(destination)) {
-			printf("Invalid destination\n");
-		}
-		else{
-			check = 1;
-			pkg->m_boxSize = size; 
-			pkg->m_weight = weight;
-			strcpy(pkg->m_destination, destination);
-		}
-	}
+void input(struct PackageInf* pkg) {
+    struct Map baseMap = populateMap();
+    struct Route blueRoute = getBlueRoute();
+    struct Map routeMap = addRoute(&baseMap, &blueRoute);
+
+    struct Route greenRoute = getGreenRoute();
+    struct Route yellowRoute = getYellowRoute();
+    routeMap = addRoute(&routeMap, &greenRoute);
+    routeMap = addRoute(&routeMap, &yellowRoute);
+
+    printMap(&routeMap, 1, 1);
+
+    int weight = 0;
+    double size = 0;
+    char dest[4] = { '\0' };
+    int stopInput = 0;
+
+    int numRow = 0;
+    char characterDest = '\0';
+
+    while (!stopInput) {
+        printf("Enter shipment weight, box size, and destination (0 0 x to stop): ");
+        int check_input = scanf("%d %lf %3s", &weight, &size, dest);  //get user input
+        checkDestInput(dest, &numRow, &characterDest);  //check if the input is valid
+
+        if (check_input != 3) {
+            printf("Input failed\n");  //if the input is not 3 values will prompt an error
+            while ((getchar()) != '\n'); // Clear input buffer
+        }
+        else if (weight == 0 && size == 0 && characterDest == 'x') {
+            stopInput = 1;  //if the input is 0 0 x will break from loop and terminate 
+        }
+        else if (!validatePackageWeight(weight)) {  //checks that pkg is a valid weight
+            printf("Invalid weight (must be 1-1000 Kg.)\n");
+        }
+        else if (!validatePackageBox(size)) {  //checks that pkg is a valid size
+            printf("Invalid size\n");
+        }
+        else if (numRow == 0 || characterDest == '\0') { //if the destination format is not valid
+            printf("Invalid destination format\n");
+        }
+        else {
+            // Destination validation against the map routes
+            int destCol = returnInt(characterDest); //converting letter to number of columns
+            if (isDestinationValid(&routeMap, numRow, destCol)) {
+
+                printf("Destination is valid. Processing package...\n"); //this is temporary just to show us that we are in a good fllow
+              
+                // Calculate the shortest route
+                struct Point start = { 0, 0 }; //setting the intial point
+                struct Point dest = { numRow, destCol }; //setting the numRow and destCol 
+                struct Route shortestRoute = shortestPath(&routeMap, start, dest);  // <<<<<<<<<<<<<<<<<<<<<<<<  issue is here
+                /*
+                based on the debugger, the moment it reaches here it crashes, check the fllow of shortestPath and you'll see
+                  addPtToRoute(struct Route* route, struct Point pt)
+                    {
+                    	route->points[route->numPoints++] = pt;
+                    }
+                */
+            }
+            else {
+                printf("Invalid destination\n"); //if the destination is not valid
+            }
+        }
+    }
 }
+
+
+void checkDestInput(const char* inputString, int* num, char* character) {
+    int numIntegers = 0;
+    char characters[3] = { '\0' }; // Maximum number of characters + 1 for null-terminator
+    int flag = 0;
+    for (int i = 0; inputString[i] != '\0' || !flag; i++) {
+        if (isdigit(inputString[i])) {
+            // Convert the substring to an integer
+            numIntegers = atoi(&inputString[i]);
+
+            // Move to the next non-digit character
+            while (isdigit(inputString[i]))
+                i++;
+
+            i--; // Decrement i to offset the increment in the loop
+        }
+        else if (isalpha(inputString[i])) {
+            characters[0] = inputString[i]; // Store only the first character
+            flag=1;
+        }
+    }
+
+    // Assign values to the variables in main
+    *num = numIntegers;
+    *character = characters[0];
+}
+
 
 
 //print out footer onto console
@@ -132,78 +157,5 @@ void footer() {
 
 
 
-struct Point setPointfromPackageInf(const struct PackageInf* pkg) {
-	char letter = toupper(pkg->m_destination);
-	int num = returnInt(letter);
-	int row = num / 5;
-	int col = num % 5;
-	return (struct Point) { row, col }; //return struct
-}
 
 
-
-struct Route calculateRoute(const struct PackageInf* pkg) {
-	//Create the map and populate it with buildings
-	struct Map map = populateMap();
-
-	//Get the appropriate route based on the package size
-	int boxSizeInt = (int)(pkg->m_boxSize * 100); // Convert to an integer with some precision
-	struct Route route;
-	switch (boxSizeInt) {
-		case (int)(SZMIN * 100) :
-			route = getBlueRoute();
-			break;
-		case (int)(SZMMID * 100) :
-			route = getGreenRoute();
-			break;
-		case (int)(SZMAX * 100) :
-			route = getYellowRoute();
-			break;
-	}
-
-	//Calculate the shortest path from the starting point to the destination
-	struct Point destination = setPointfromPackageInf(pkg);
-	struct Route shortestPathRoute = shortestPath(&map, route.points[0], destination);
-
-	//Add the shortest path to the original route
-	for (int i = 1; i < shortestPathRoute.numPoints; i++) {
-		addPtToRoute(&route, shortestPathRoute.points[i]);
-	}
-
-	//Return the final route
-	return route;
-}
-
-
-// Combine functions so that they can be used and print output (as instructions)
-void printInstructions(struct PackageInf* pkg, struct Map* map) {
-	//Display the chosen package details
-	printf("Shipment details:\n");
-	printf("Weight: %.2lf Kg\n", pkg->m_weight);
-	printf("Box Size: %.2lf\n", pkg->m_boxSize);
-	printf("Destination: %s\n", pkg->m_destination);
-
-	// Determine the route chosen
-	char routeSymbol = map->squares[0][0]; // The starting point of the route is always (0, 0)
-	switch (routeSymbol) {
-	case BLUE:
-		printf("Ship on BLUE LINE, no diversion\n");
-		break;
-	case GREEN:
-		printf("Ship on GREEN LINE, no diversion\n");
-		break;
-	case YELLOW:
-		printf("Ship on YELLOW LINE, no diversion\n");
-		break;
-	case DIVERSION:
-		printf("Ship on DIVERSION LINE, route diverted\n");
-		break;
-	default:
-		printf("Unknown route\n");
-		break;
-	}
-
-	// Display the map with the chosen route
-	printf("\nMap with the chosen route:\n");
-	printMap(map, 1, 1);
-}
--- SourceCode/project-starter/input.h
+++ SourceCode/project-starter/input.h
@@ -39,22 +39,24 @@ void footer();
 //get user input to enter package data related to weight, boxsize and destination, "0 0 x" to stop
 void input(struct PackageInf *);
 
+//shortes route
+struct Route calculateShortestRoute(const struct Map* map, const struct Point start, const struct Point dest);
+
+//checks the destination input, and breaks it into an int and char
+int isDestinationValid(const struct Map* routeMap, int destRow, char destCol);
+void checkDestInput(const char* inputString, int* num, char* character);
+
 //returns 1 weight if package within limits (1-1000), else 0
 int validatePackageWeight(double);
 
 //returns 1 if package within cubic meter limits (0.25/0.5/1), else 0
 int validatePackageBox(double);
 
-//returns 1 if destination  is valid(if the destination correspnds to the allowed destinations in the array, ('X' = building re populateMap() for location of buildings from 'struct Map' ), else return 0
-int validateDestination(char* dest); 
+
 
 //Combine functions so that they can be used and print output (as instructions)
-void printInstructions(struct PackageInf* pkg, struct Map* map);
+//void printInstructions(struct PackageInf* pkg, struct Map* map);   //not done as it crashes before we can read that point
 
-//set point from PackageInfo (from what we got from user)
-struct Point setPointfromPackageInf(const struct PackageInf *);
 
-//calculating route
-struct Route calculateRoute(const struct PackageInf* pkg);
 
 #endif
