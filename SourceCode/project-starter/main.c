038fa0a9e7b38b3a4b2f72508d4e04f4e47936f0
JulAleks <jalekseev@myseneca.ca>
2023-07-24 14:15:09 -0400

    NEW MAIN

--- SourceCode/project-starter/main.c
+++ SourceCode/project-starter/main.c
@@ -15,47 +15,20 @@ Group members:
 #include "mapping.h"
 #include "input.h"
 
-int main(void)
-{
-	struct Map baseMap = populateMap();
-	struct Route blueRoute = getBlueRoute();
-	struct Map routeMap = addRoute(&baseMap, &blueRoute);
+int main(void) {
 
-	// MS2 Newly added code:
-	// for getting Green and Yellow Route showing on the map:
-	struct Route greenRoute = getGreenRoute();
-	struct Route yellowRoute = getYellowRoute();
 
-	routeMap = addRoute(&routeMap, &greenRoute);
-	routeMap = addRoute(&routeMap, &yellowRoute);
+    //print header
+    header();
 
-	// printMap(&routeMap, 1, 1);
+    //creating a pkg struct 
+    struct PackageInf pkg;
 
-	// initialize start point
-	struct Point startPoint = { 0, 0 };
+    //invoking input
+    input(&pkg);
 
-	//initialize PackageInf struct to store input data
-	struct PackageInf pkg;
+    //print footer
+    footer();
 
-	//print header
-	header();
-
-	int flag = 1;
-	while (flag) {
-		input(&pkg);
-
-		//set destination point based on what we got from input function
-		struct Point destPoint = setPointfromPackageInf(&pkg);
-
-		//Initialize the shortest route from start point to destination Point
-		struct Route shortestRoute = shortestPath(&routeMap, startPoint, destPoint);
-
-		//checks if user input '0 0 x' to exit.
-		if(pkg.m_weight == 0 && pkg.m_boxSize == 0 && pkg.m_destination[0] == 'x') {
-			flag = 0;
-		}
-	}
-	//print footer
-	footer();
-	return 0;
+    return 0;
 }
