c044162c39e0fad10c14fd78350a6e3ff3e1e0ac
audreylearns <audreyduzon.cda@GMAIL.COM>
2023-07-25 17:43:07 -0400

    Integration test environment added

---
+++ SourceCode/Integration_Test/Integration_Test.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************
+//SFT GROUP 5 PROJECT
+
+Group members:
+152953220 - TAE YONG EOM - tyeom@myseneca.ca
+051292134 - Julia Alekseev  - jalekseev@myseneca.ca
+123231227 - Chan Ka Ying - kchan151@myseneca.ca
+1122117229 - Azad Zeynalov - azeynalov1@myseneca.ca
+019153147 - Audrey Duzon - amduzon@myseneca.ca
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "input_r.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace IntegrationTest
+{
+	TEST_CLASS(IntegrationTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
