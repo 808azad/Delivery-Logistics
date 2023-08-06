ff605876bbc4fba1b8ee138c0874878a5cb978b3
Azad Zeynalov <azeynalov1@myseneca.ca>
2023-08-06 01:20:56 -0400

    Test case 7 developed

--- SourceCode/Integration_test_azad/Integration_Test_azad/IntegrationTestingMain.c
+++ SourceCode/Integration_test_azad/Integration_Test_azad/IntegrationTestingMain.c
@@ -80,6 +80,16 @@ int main() {
 	footer();
 
 
+	printf("\n\n\nCase 7: Testing the program for proper shipment on the blue line with diversion (using the same values as in case 6).\n");
+	weight = 500;
+	size = 1.0;
+	strcpy(dest, "8Y");
+	printf("Values that will be used: %d %.1lf %s\n\n", weight, size, dest);
+	header();
+	input(weight, size, dest);
+	footer();
+
+	
 
 	return 0;
 }
